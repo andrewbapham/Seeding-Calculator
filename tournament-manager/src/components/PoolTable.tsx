2cc840aeb50b9377538e1b3e630ceb9ef24946ea
Andrew Pham <andrewbapham@outlook.com>
2023-08-10 18:45:22 -0400

    Add calculation for sets won and point differential

--- tournament-manager/src/components/PoolTable.tsx
+++ tournament-manager/src/components/PoolTable.tsx
@@ -10,6 +10,9 @@ interface Team {
   teamNumber: number;
 }
 
+const singleScoreRe = /^\d{1,2}-\d{1,2}$/;
+const twoScoreRe = /^\d{1,2}-\d{1,2},\s?\d{1,2}-\d{1,2}$/;
+
 // Table of scores for a pool. The table is organized so that the rows and columns represent the teams,
 // and the cells represent the scores of the matches between the teams.
 // The diagonal of the table is empty, since a team cannot play against itself.
@@ -18,7 +21,7 @@ const PoolTable = (props: PoolTableProps) => {
   for (let i = 0; i < props.pool.length; i++) {
     defaultScores.push([]);
     for (let j = 0; j < props.pool.length; j++) {
-      defaultScores[i].push("2-0");
+      defaultScores[i].push("");
     }
   }
 
@@ -41,6 +44,8 @@ const PoolTable = (props: PoolTableProps) => {
     });
   });
 
+  useEffect(() => {}, [scores]);
+
   //When a score is changed, update the scores array, and update the table including the related game which will have the opposite score
   const handleScoreChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newScores = [...scores];
@@ -54,16 +59,14 @@ const PoolTable = (props: PoolTableProps) => {
 
     newScores[rowIndex][colIndex] = event.target.value;
 
-    const singleScoreRe = /^\d{1,2}-\d{1,2}$/;
+    //TODO use this code to set sets won and point differential also?
     if (singleScoreRe.test(event.target.value)) {
       const split = event.target.value.split("-");
-      console.log(split);
       newScores[colIndex][rowIndex] = split[1] + "-" + split[0];
     }
 
-    const twoScoreRe = /^\d{1,2}-\d{1,2}, \d{1,2}-\d{1,2}$/;
     if (twoScoreRe.test(event.target.value)) {
-      const split = event.target.value.split(", ");
+      const split = event.target.value.split(",");
       const game1 = split[0].split("-");
       const game2 = split[1].split("-");
       newScores[colIndex][rowIndex] =
@@ -82,6 +85,8 @@ const PoolTable = (props: PoolTableProps) => {
           <tr>
             <th></th>
             {teamNameHeaders}
+            <th>Sets Won</th>
+            <th>Point Differential</th>
           </tr>
         </thead>
         <tbody>
@@ -99,10 +104,40 @@ const PoolTable = (props: PoolTableProps) => {
               );
             }
 
+            let setsWon = 0;
+            let pointDifferential = 0;
+
+            for (let i = 0; i < props.pool.length; i++) {
+              if (singleScoreRe.test(scores[index][i])) {
+                const split = scores[index][i].split(",");
+                const game1 = split[0].split("-");
+                const diff = parseInt(game1[0]) - parseInt(game1[1]);
+                setsWon += diff > 0 ? 1 : 0;
+                pointDifferential += diff;
+              }
+              if (twoScoreRe.test(scores[index][i])) {
+                const split = scores[index][i].split(",");
+                const game1 = split[0].split("-");
+                const diff1 = parseInt(game1[0]) - parseInt(game1[1]);
+                setsWon += diff1 > 0 ? 1 : 0;
+                pointDifferential += diff1;
+
+                const game2 = split[1].split("-");
+                const diff2 = parseInt(game2[0]) - parseInt(game2[1]);
+                pointDifferential += diff2;
+                setsWon += diff2 > 0 ? 1 : 0;
+              }
+            }
+
+            const setsWonElement = <td>{setsWon}</td>;
+            const pointDifferentialElement = <td>{pointDifferential}</td>;
+
             return (
               <tr key={team.name}>
                 <td>{team.name}</td>
                 {inputCells}
+                {setsWonElement}
+                {pointDifferentialElement}
               </tr>
             );
           })}
